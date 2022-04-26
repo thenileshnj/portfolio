a352e6678d0882c7e9d30c8f8f33c7a0ced50ee7
Nilesh Nj <nileshbjadhawar@gmail.com>
2022-04-26 23:13:01 +0530

    few imp modifications

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaLinkedin, FaInstagram, FaGithub, FaFacebook } from 'react-icons/fa';
+import { FaLinkedin, FaMailBulk, FaWhatsapp, FaInstagram, FaGithub, FaFacebook } from 'react-icons/fa';
 
 const Contact = () => {
   return (
@@ -12,11 +12,13 @@ const Contact = () => {
           <p>Hey, Want to Hire me or Discus about any Project </p>
         </div>
         <div className='flex mx-auto mb-5'>
-          <div className='flex px-12 py-12 mx-auto justify-center bg-black text-white rounded-3xl shadow-2xl shadow-black'>
+          <div className='flex px-12 py-12 mx-auto justify-center font-bold bg-black text-white rounded-3xl shadow-2xl shadow-black'>
             <div className='flex justify-center border-2 p-4 rounded-xl'>
               <div className='mr-4 items-center'>nileshbjadhawar@gmail.com</div>
-              <div className='bg-yellow-500 rounded-3xl p-2'>
-                <a target="_blank" href="mailto:nileshbjadhawar@gmail.com">Mail ME</a>
+              <div className='bg-yellow-500 rounded-3xl p-2 font-extrabold'>
+                <a target="_blank" href="mailto:nileshbjadhawar@gmail.com">
+                <FaMailBulk className='font-extrabold'/>
+                </a>
               </div>
             </div>
           </div>
@@ -27,11 +29,13 @@ const Contact = () => {
         </div>
 
         <div className='flex mx-auto mb-5'>
-          <div className='flex px-12 py-12 mx-auto justify-center bg-black text-white rounded-3xl shadow-2xl shadow-black'>
+          <div className='flex px-12 py-12 mx-auto justify-center font-bold bg-black text-white rounded-3xl shadow-2xl shadow-black'>
             <div className='flex justify-center border-2 p-4 rounded-xl'>
               <div className='mr-4 items-center'>+91 8308885149</div>
-              <div className='bg-yellow-500 rounded-3xl p-2'>
-                <a target="_blank" href="https://wa.me/message/Y37N46MVEZIZC1">WhatsApp Me</a>
+              <div className='bg-yellow-500 font-extrabold rounded-3xl p-2'>
+                <a target="_blank" href="https://wa.me/message/Y37N46MVEZIZC1">
+                <FaWhatsapp/>
+                </a>
               </div>
             </div>
           </div>
--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import avatar from "../assets/avatar.svg"
 import { FaLinkedinIn, FaGithub, FaInstagram, FaFacebook } from 'react-icons/fa';
 
 const ProfileCard = () => {
@@ -6,7 +7,7 @@ const ProfileCard = () => {
     <div className='flex mx-auto justify-center pt-16 mb-5'>
         <div className='rounded-3xl px-14 py-5 md:px-24 md:py-8 shadow-xl bg-white'>
             <div className='flex justify-center items-center'>
-                <img src="avatar.svg" alt="Nilesh Nj Profile" />
+                <img src={avatar} alt="Nilesh Nj Profile" />
             </div>
             <div className='text-center mt-4'>
                 <p className='font-extrabold text-2xl'>Nilesh Nj</p>
